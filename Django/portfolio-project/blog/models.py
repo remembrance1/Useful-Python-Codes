5a650058dc8bb39d126af37df86b4eb14e7083a2
remembrance1 <javierngkh@gmail.com>
2019-03-27 15:33:33 +0800

    add blog

--- Django/portfolio-project/blog/models.py
+++ Django/portfolio-project/blog/models.py
@@ -1,4 +1,7 @@
 from django.db import models
 
-class Blog():
-    
+class Blog(models.Model):
+    title = models.CharField(max_length = 255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
