8af0bc0337db151776ddedd1b6e95e20721a5ba1
remembrance1 <javierngkh@gmail.com>
2019-03-27 15:27:04 +0800

    migrated to postgresql

--- Django/portfolio-project/jobs/models.py
+++ Django/portfolio-project/jobs/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model): #create a new class in py called Job
+    image = models.ImageField(upload_to = 'images/') #image uploaded shld be sent to this folder
+    summary = models.CharField(max_length = 200)
